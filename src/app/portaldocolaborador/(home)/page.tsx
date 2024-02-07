9eace0de5cafb97679c1bfc5130a26eef4b92dc1
funerariavida <ti@funerariavida.com.br>
2024-02-07 14:41:51 -0300

    style: utilities layout responsive

--- src/app/portaldocolaborador/(home)/page.tsx
+++ src/app/portaldocolaborador/(home)/page.tsx
@@ -6,13 +6,14 @@ export default function PortalColaborador() {
   return (
     <div className="flex flex-col items-center justify-center w-full min-h-screen">
       {/* Utils section */}
-      <section className="max-[400px]:h-[160vh] h-[170vh] max-[782px]:h-[180vh] min-[782px]:h-[130vh] gap-5 sm:gap-10 w-full flex flex-col items-center justify-start">
-        <div className="w-full bg-primary h-[12%] md:h-[15%] lg:h-1/4 flex flex-col items-center justify-center">
+      {/* <section className="max-[400px]:h-[160vh] h-[170vh] max-[782px]:h-[180vh] min-[782px]:h-[130vh] gap-5 sm:gap-10 w-full flex flex-col items-center justify-start"> */}
+      <section className="h-full w-full flex flex-col items-between justify-start">
+        <div className="w-full bg-primary h-20 md:h-28 lg:h-32 flex flex-col items-center justify-center">
           <h1 className="text-2xl font-bold text-white sm:text-4xl lg:text-5xl">
             Portal do Colaborador
           </h1>
         </div>
-        <div className="flex flex-wrap items-center justify-center w-full gap-4">
+        <div className="px-2 py-3 flex flex-wrap items-center justify-center w-full gap-3">
           <div className="w-full text-center">
             <h3 className="text-lg font-semibold lg:text-2xl text-secondary">
               Utilitários
