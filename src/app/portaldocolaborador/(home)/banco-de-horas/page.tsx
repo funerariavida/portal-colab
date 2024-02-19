e69ae5c14095c9b145e94167579600f967dfdbe5
victor <victorgv.dev@gmail.com>
2024-02-19 09:12:48 -0300

    fix: error before deploy

--- src/app/portaldocolaborador/(home)/banco-de-horas/page.tsx
+++ src/app/portaldocolaborador/(home)/banco-de-horas/page.tsx
@@ -1,9 +1,5 @@
-import getCompTime from '@/functions/get-comp-time'
-
 export default async function BancoPage() {
-  const data = await getCompTime('VICTOR JERRYSSON GAMA BASTOS')
-
-  console.log(data)
+  // const data = await getCompTime('VICTOR JERRYSSON GAMA BASTOS')
 
   return <div>BancoPage</div>
 }
