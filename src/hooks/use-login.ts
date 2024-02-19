91433b79974e8f1b3408a7fc5e048f94918b4c54
victor <victorgv.dev@gmail.com>
2024-02-19 10:47:20 -0300

    fix: deploy bug

--- src/hooks/use-login.ts
+++ src/hooks/use-login.ts
@@ -33,7 +33,7 @@ export default function useLogin() {
       setIsLoading(true)
 
       axios<CollabResponse>({
-        url: `www.empresavida.com.br/api/login`,
+        url: `${process.env.NEXT_PUBLIC_NODE_API_BASE_URL}/login`,
         method: 'post',
         data: {
           cpf,
