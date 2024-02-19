c8f4b36f0f4fa919dbbbcd3990ceb8f8a0385a72
victor <victorgv.dev@gmail.com>
2024-02-19 15:10:12 -0300

    fix: Api clal bug!

--- src/app/api/links/route.ts
+++ src/app/api/links/route.ts
@@ -3,16 +3,15 @@ import { NextResponse } from 'next/server'
 
 export async function GET() {
   const res = await fetch(`${process.env.API_BASE_URL}/content`, {
-    cache: 'no-cache',
-    next: {
-      revalidate: 2000,
-    },
+    cache: 'no-store',
     headers: {
       token: process.env.API_CONTENT_KEY,
     },
   })
 
   const data: LinkProps[] = await res.json()
+
+  console.log(data[0])
 
   return NextResponse.json({
     data,
