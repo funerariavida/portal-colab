01315e5d52d1dacb9adcb897afb5f9ca0a198643
victor <victorgv.dev@gmail.com>
2024-02-19 10:31:38 -0300

    fix: deploy bug

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -3,14 +3,17 @@ import { NextResponse } from 'next/server'
 export async function POST(request: Request) {
   const req = await request.json()
 
-  const data = await fetch(`${process.env.API_BASE_URL}/auth`, {
-    method: 'post',
-    headers: {
-      'Content-Type': 'application/json',
-      token: process.env.API_LOGIN_KEY,
+  const data = await fetch(
+    `https://n8n.empresavida.com.br/webhook/api/portal/auth`,
+    {
+      method: 'post',
+      headers: {
+        'Content-Type': 'application/json',
+        token: process.env.API_LOGIN_KEY,
+      },
+      body: JSON.stringify(req),
     },
-    body: JSON.stringify(req),
-  }).then((res) => res.json())
+  ).then((res) => res.json())
 
   return NextResponse.json({
     data,
