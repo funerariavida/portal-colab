a318f9cf2c7e2d3aa97338c40d44f066ef39e3e2
victor <victorgv.dev@gmail.com>
2024-02-19 16:42:46 -0300

    feat: Header link

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -2,15 +2,16 @@ import Image from 'next/image'
 
 import Logo from '@/assets/image/logo.png'
 
+import Link from 'next/link'
 import HeaderInfo from './header-info'
 import HeaderSheet from './header-sheet'
 
 export default function Header() {
   return (
     <header className="fixed top-0 z-10 flex items-center justify-center w-full h-20 px-3 py-6 shadow-md bg-background lg:px-5">
-      <div className="w-32 ms-7 lg:m-0">
+      <Link href={'/'} className="w-32 ms-7 lg:m-0">
         <Image className="w-full" src={Logo} alt="Logomarca" />
-      </div>
+      </Link>
       <HeaderInfo className="flex items-center justify-center h-full gap-4 ms-auto" />
       <HeaderSheet />
     </header>
