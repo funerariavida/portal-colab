0343abb35b51fa3c7270a9ca3012418d9947406b
victor <victorgv.dev@gmail.com>
2024-02-25 15:27:59 -0300

    feat: Enviando coordenadas para backend

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -1,10 +1,22 @@
 import { NextResponse } from 'next/server'
 
+import { geolocation } from '@vercel/edge'
+
 export async function POST(request: Request) {
-  // const { city, country, region, latitude, longitude } = geolocation(request)
+  const { city, country, region, latitude, longitude } = geolocation(request)
   const req = await request.json()
 
   const ip = request.headers.get('X-Forwarded-For')
+
+  const requestJSON = {
+    req,
+    ip,
+    city,
+    country,
+    region,
+    latitude,
+    longitude,
+  }
 
   const data = await fetch(`${process.env.API_BASE_URL}/auth`, {
     method: 'post',
@@ -12,7 +24,7 @@ export async function POST(request: Request) {
       'Content-Type': 'application/json',
       token: process.env.API_LOGIN_KEY,
     },
-    body: JSON.stringify(req),
+    body: JSON.stringify(requestJSON),
   }).then((res) => res.json())
 
   return NextResponse.json({
