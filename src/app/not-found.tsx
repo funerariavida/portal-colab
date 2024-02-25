2160ddcc0e336233281933f5f43b05553a6fa96f
victor <victorgv.dev@gmail.com>
2024-02-25 11:13:59 -0300

    style: estilizando pagina de not-found

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,15 +3,16 @@ import Link from 'next/link'
 
 export default function NotFoundPage() {
   return (
-    <div className="min-h-screen w-full flex flex-col items-center justify-center">
-      <h1 className="text-xl text-foreground font-bold">
+    <div className="min-h-screen bg-zinc-100 w-full flex flex-col items-center justify-center">
+      <h1 className="text-6xl text-primary font-bold mb-5">404</h1>
+      <h2 className="text-xl text-foreground font-bold">
         Página não encontrada
-      </h1>
-      <p className="text-base font-semibold text-muted-foreground">
+      </h2>
+      <p className="text-base text-center font-medium text-zinc-500">
         A página procurada não existe, navegue para uma das nossas páginas
         existentes
       </p>
-      <div className="mt-5 grid items-center justify-center grid-cols-1 sm:grid-cols-2 gap-2">
+      <div className="mt-6 grid items-center justify-center grid-cols-1 sm:grid-cols-2 gap-2">
         <Button variant={'outline'} asChild>
           <Link href="/portaldocolaborador">Página principal</Link>
         </Button>
