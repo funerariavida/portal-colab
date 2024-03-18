2daf7c336a21a44a9c892e8291683d3845c6fdf8
victor <victorgv.dev@gmail.com>
2024-03-18 16:49:37 -0300

    feat: Criando switch para formatação de titulo de setor

--- src/app/portaldocolaborador/(home)/setores/[setor]/page.tsx
+++ src/app/portaldocolaborador/(home)/setores/[setor]/page.tsx
@@ -11,31 +11,54 @@ type routeParams = {
   }
 }
 
+function formatDepartment(department: string): string {
+  switch (department) {
+    case 'rh': {
+      return 'Recursos Humanos'
+      break
+    }
+    case 'dp': {
+      return 'Departamento Pessoal'
+      break
+    }
+    case 't.i': {
+      return 'Tecnologia da Informação'
+      break
+    }
+    case 'assistencia': {
+      return 'Assistência Familiar'
+      break
+    }
+    case 'cobranca': {
+      return 'Cobrança'
+      break
+    }
+    case 'cpd': {
+      return 'Centro de Processamento de Dados'
+      break
+    }
+    default: {
+      return department
+      break
+    }
+  }
+}
+
 export default function AtendimentoPage({ params }: routeParams) {
   const department =
     params.setor === 'ti' ? 't.i' : params.setor.split('_').join(' ')
   const { getLinkByPage } = useLinkContext()
   const data = getLinkByPage(department)
 
-  let formatedDepartment = department
-
-  if (department === 'rh') {
-    formatedDepartment = 'Recursos Humanos'
-  } else if (department === 'dp') {
-    formatedDepartment = 'Departamento Pessoal'
-  } else if (department === 't.i') {
-    formatedDepartment = 'Tecnologia da Informação'
-  } else if (department === 'assistencia') {
-    formatedDepartment = 'Assistência Familiar'
-  }
+  const formatedDepartment = formatDepartment(department)
 
   if (!data) return <CardGroupSkeleton />
 
   console.log(formatedDepartment, params.setor)
 
   return (
-    <div className="mx-auto grid w-full h-full max-w-[1440px] grid-rows-[min_content_max-content]">
-      <div className="w-full bg-secondary h-32 flex flex-col items-center justify-center">
+    <div className="mx-auto grid min-w-full min-h-full grid-rows-[max-content_auto]">
+      <div className="w-full bg-secondary py-10 flex flex-col items-center justify-center">
         <h1 className="text-2xl font-bold text-white capitalize">
           {formatedDepartment}
         </h1>
