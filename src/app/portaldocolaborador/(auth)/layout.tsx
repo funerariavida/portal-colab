beed538622a8ef25ff98ef1748195327d2f427ac
victor <victorgv.dev@gmail.com>
2024-03-18 16:23:52 -0300

    style: Refatorando layout grid do login

--- src/app/portaldocolaborador/(auth)/layout.tsx
+++ src/app/portaldocolaborador/(auth)/layout.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from 'react'
 
 export default function AuthLayout({ children }: { children: ReactNode }) {
   return (
-    <div className="flex w-full min-h-screen items-center justify-center">
+    <div className="flex h-screen w-full max-w-screen-desktop-xl max-h-[1080px] items-center justify-center">
       {children}
     </div>
   )
