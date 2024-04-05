3397f2636696d258113af73dcdd6ca02c5f44e1a
VictorJGB <victorgb.dev@gmail.com>
2024-04-05 10:58:55 -0300

    refactor: news dialog image render size

--- src/components/dialog/news-dialog.tsx
+++ src/components/dialog/news-dialog.tsx
@@ -117,8 +117,8 @@ export default function NewsDialog({
                 src={require('@/assets/image/news/image-01.png')}
                 alt="news-image"
                 className="max-w-full"
-                width={600}
-                height={600}
+                width={100}
+                height={100}
               />
             </div>
             <div className="max-w-[600px] border border-zinc-300">
@@ -126,8 +126,8 @@ export default function NewsDialog({
                 src={require('@/assets/image/news/image-02.png')}
                 alt="news-image"
                 className="max-w-full"
-                width={600}
-                height={600}
+                width={100}
+                height={100}
               />
             </div>
           </div>
