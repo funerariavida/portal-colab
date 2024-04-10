7bc7e5c844d566248365e1ddf5bff918de6a1689
VictorJGB <victorgb.dev@gmail.com>
2024-04-10 10:49:52 -0300

    feat: Implementando componente de setor na home page

--- src/app/portaldocolaborador/(home)/page.tsx
+++ src/app/portaldocolaborador/(home)/page.tsx
@@ -1,13 +1,20 @@
-// cookies
+'use client'
+
+// context
+import { useLinkContext } from '@/context/link'
 
 // components
-import DepartmentSection from '@/components/sections/department'
-import TrainingSection from '@/components/sections/training'
-import UtilsSection from '@/components/sections/utilities'
+import GenericSector from '@/components/generic-sector'
+import CardGroupSkeleton from '@/components/skeleton/card-group'
 
 export default function PortalPage() {
+  const { getLinkByPage } = useLinkContext()
+  const data = getLinkByPage('main')
+
+  if (!data) return <CardGroupSkeleton />
+
   return (
-    <div className="grid min-h-screen min-w-full grid-cols-1 grid-rows-[max-content_auto_auto_auto]">
+    <div className="grid min-h-screen min-w-full grid-cols-1">
       {/* title */}
       <div className="cols-span-1 grid w-full place-items-center bg-primary py-10">
         <h1 className="text-heading-3 font-bold text-white md:text-heading-2 lg:text-heading-1">
@@ -16,13 +23,16 @@ export default function PortalPage() {
       </div>
 
       {/* Utils section */}
-      <UtilsSection />
+      {/* <UtilsSection /> */}
+      <GenericSector link={data[0]} variant="primary" hasHeader />
 
       {/* Department section */}
-      <DepartmentSection />
+      {/* <DepartmentSection /> */}
+      <GenericSector link={data[2]} variant="default-secondary" hasHeader />
 
       {/* Training section */}
-      <TrainingSection />
+      {/* <TrainingSection /> */}
+      <GenericSector link={data[1]} variant="default-primary" hasHeader />
     </div>
   )
 }
