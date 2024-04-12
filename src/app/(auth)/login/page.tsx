bd1e80d761118685addb523aae3655c0151691d9
VictorJGB <victorgb.dev@gmail.com>
2024-04-12 16:00:11 -0300

    style: adicionando logo e favicon

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,10 +1,6 @@
 // next
 import Image from 'next/image'
 
-// assets
-import Logo from '@/assets/logo/logo-primary.png'
-import Logo02 from '@/assets/logo/logo-secondary.png'
-
 // components
 import LoginForm from '@/components/login-form'
 
@@ -14,14 +10,26 @@ export default function LoginPage() {
       {/* first part */}
       <div className="hidden min-h-full w-1/2 items-center justify-center text-primary md:flex">
         <div className="w-52">
-          <Image className="w-full" src={Logo} alt="Logomarca" />
+          <Image
+            className="w-full"
+            src={'/logo/logo.svg'}
+            alt="Logomarca"
+            width={100}
+            height={100}
+          />
         </div>
       </div>
 
       {/* second part */}
       <div className="flex min-h-full w-full flex-col items-center justify-center gap-5 bg-primary text-white md:w-1/2">
         <div className="mb-2 flex w-1/2 p-4 md:hidden">
-          <Image src={Logo02} alt="logomarca" />
+          <Image
+            className="w-full"
+            src={'/logo/logo_white.svg'}
+            alt="logomarca"
+            width={100}
+            height={100}
+          />
         </div>
         <div className="flex flex-col items-center justify-center gap-1">
           <h1 className="text-xl font-bold text-white">Login</h1>
--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -3,7 +3,6 @@ import Image from 'next/image'
 import Link from 'next/link'
 
 // assets
-import Logo from '@/assets/logo/logo-primary.png'
 
 // header components
 import HeaderInfo from './header-info'
@@ -11,9 +10,15 @@ import HeaderSheet from './header-sheet'
 
 export default function Header() {
   return (
-    <header className="fixed top-0 z-10 flex h-20 w-full max-w-[1920px] items-center justify-center bg-background px-3 py-6 shadow-md lg:px-5">
+    <header className="fixed top-0 z-10 flex h-20 w-full max-w-[1920px] items-center justify-center bg-background px-5 py-6 shadow-md lg:px-5">
       <Link href={'/'} className="ms-7 w-32 lg:m-0">
-        <Image className="w-full" src={Logo} alt="Logomarca" />
+        <Image
+          className="w-full"
+          src={'/logo/logo.svg'}
+          alt="Logomarca"
+          width={100}
+          height={100}
+        />
       </Link>
       <HeaderInfo className="ms-auto flex h-full items-center justify-center gap-4" />
       <HeaderSheet />
